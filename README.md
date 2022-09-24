d957c13996edbb9bc311b066c196f8500a4ac10a
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2022-09-24 14:30:07 +0700

    `arithmexp`

--- README.md
+++ README.md
@@ -1,5 +1,47 @@
-# Calculator
-This code is for reference only. Absolutely do not use in production!
+# General
+**The plugin allows to execute math operations in the server or console with `/calculator` command**
+
+# Arithmetic Operators
+List of supported arithmetic operators: `/`, `*`, `+`, `-`.  
+Description of arithmetic operators: https://www.php.net/manual/en/math.constants.php
+
+# Constants
+List of supported constants: `e`, `log2e`, `log10e`, `ln2`, `ln10`, `pi`, `euler`.  
+Description and values of functions: https://www.php.net/manual/en/math.constants.php
+
+# Commands & Permissions
+```yaml
+commands:
+  calculator:
+    description: "Perform a math operation"
+    usage: "/calculator <expression>"
+    permission: calculator.command
+    aliases: ["calc"]
+permissions:
+  calculator.command:
+    default: true
+    description: "Allows the use of /calculator"
+```
+
+# Config
+```yaml
+---
+# Prefix of messages.
+prefix: "&f[&6Calculator&f]&r"
+
+# {prefix} : Prefix.
+# {result} : The result of the math.
+result: "{prefix} &aResult: &b{result}"
+
+# {prefix} : Prefix.
+# {error} : Error information.
+error: "{prefix} &cError: {error}"
+
+# playSound: true > A sound will be sent to the player when a math operation is performed.
+playSound: true
+...
+
+```
 
 # Contact
 [![Discord](https://img.shields.io/discord/986553214889517088?label=discord&color=7289DA&logo=discord)](https://discord.gg/j2X83ujT6c)\
--- src/NhanAZ/Calculator/Main.php
+++ src/NhanAZ/Calculator/Main.php
@@ -4,31 +4,44 @@ declare(strict_types=1);
 
 namespace NhanAZ\Calculator;
 
+use muqsit\arithmexp\Parser;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\network\mcpe\protocol\PlaySoundPacket;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
 
 class Main extends PluginBase {
 
-	const PREFIX = "§f[§bCalculator§f]§r ";
-
 	protected function onEnable(): void {
-		$this->getLogger()->emergency("Absolutely do not use this plugin in production!");
+		$this->saveDefaultConfig();
+	}
+
+	private function playSound($player, string $soundName): void {
+		if ($this->getConfig()->get("playSound")) {
+			if ($player instanceof Player) {
+				$playerPos = $player->getPosition();
+				$player->getNetworkSession()->sendDataPacket(PlaySoundPacket::create($soundName, $playerPos->getX(), $playerPos->getY(), $playerPos->getZ(), 1, 1));
+			}
+		}
 	}
 
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
 		if ($command->getName() === "calculator" && count($args) >= 1) {
-			$hanlder = implode("", $args);
-			var_dump($hanlder);
-			$this->getLogger()->alert("Executing PHP: $hanlder");
+			$prefix = $this->getConfig()->get("prefix");
+			$result = $this->getConfig()->get("result");
+			$error = $this->getConfig()->get("error");
 			try {
-				$rerult = eval("return $hanlder;");
-				if (is_numeric($rerult)) {
-					$sender->sendMessage(self::PREFIX . "§aResult: " . number_format($rerult, 2));
-				}
+				$parser = Parser::createDefault();
+				$expression = $parser->parse(implode("", $args));
+				$result = str_replace(["{prefix}", "{result}"], [$prefix, $expression->evaluate()], $result);
+				$sender->sendMessage(TextFormat::colorize($result));
+				$this->playSound($sender, "mob.villager.yes");
 			} catch (\Throwable $e) {
-				$sender->sendMessage(self::PREFIX . "§cError: " . $e->getMessage());
-				return true;
+				$error = str_replace(["{prefix}", "{error}"], [$prefix, $e->getMessage()], $error);
+				$sender->sendMessage(TextFormat::colorize($error));
+				$this->playSound($sender, "mob.villager.no");
 			}
 			return true;
 		}
